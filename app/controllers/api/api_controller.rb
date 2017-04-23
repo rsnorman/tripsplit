e95b7c73e4eb882d4f4f73ae73423159b3b34400
Ryan Norman <rnorman@covermymeds.com>
2017-04-23 09:29:05 -0400

    Returns unauthorized error when rescuing from cancan

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,3 +1,7 @@
 class Api::ApiController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  rescue_from CanCan::AccessDenied do
+    render status: :unauthorized
+  end
 end
