a188cd151745049acb0f4faa946cb9e1a6bb3073
Ryan Norman <rnorman@covermymeds.com>
2017-04-30 08:19:40 -0400

    Always redirects to ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_to_https
   before_action :authenticate_user!, unless: :devise_controller?
-
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def redirect_to_https
+    redirect_to protocol: 'https://' unless (request.ssl? || request.local?)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:password, :password_confirmation, :current_password])
